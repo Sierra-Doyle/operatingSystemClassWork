42bd476ff96dbbdb0540c865ab84ec113546865e
Sierra-Doyle <smdoyle2004@gmail.com>
2025-01-17 13:58:18 -0500

    Jan 17th in class exercises 1 and 2

---
+++ jan17Exercise1.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+void main()
+{   
+    // Print 10 to 1 then Happy New Year
+    int i = 10;
+    while(i > 0){
+        printf("%d\n", i);
+        i--;
+    }
+    printf("Happy New Year!\n");
+
+}
---
+++ jan17Exercise2.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+void main()
+{   
+    // Print all even numbers from 0 10 10
+    int i = 0;
+    while(i < 11){
+        if(i % 2 ==0){
+            printf("%d\n", i);
+        }
+        i++;
+    }
+
+}
