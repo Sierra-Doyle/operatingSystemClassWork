ea1fbecfbf230dce56dc06ccb4e3ba0a2dcaf03d
Sierra-Doyle <smdoyle2004@gmail.com>
2025-01-17 14:11:48 -0500

    Fixed multiplication table

--- jan17Exercise4.c
+++ jan17Exercise4.c
@@ -5,8 +5,9 @@ void main()
 
     for(i = 1; i <= 9; i++){
         for(j = 1; j<= 9; ++j){
-            printf("%d * %d = %d\n", i, j, i*j); 
+            printf("%d * %d = %d\t", j, i, i*j); 
         }
+        printf("\n");
     }
 
 
